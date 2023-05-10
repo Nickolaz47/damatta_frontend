092611a33d132759ce7ce36dddc6ce0efe1f7684
Nicolas da Matta <nicolasaraujo47@gmail.com>
2023-05-10 17:35:26 -0300

    Updated api config.

--- src/redux/config/requestConfig.ts
+++ src/redux/config/requestConfig.ts
@@ -1,7 +1,9 @@
 const { REACT_APP_ENV, REACT_APP_DEV_API_URL, REACT_APP_PROD_API_URL } =
   process.env;
 export const baseUrl: string | undefined =
-  REACT_APP_ENV === "dev" ? REACT_APP_DEV_API_URL : REACT_APP_PROD_API_URL;
+  REACT_APP_ENV === "dev"
+    ? `${REACT_APP_DEV_API_URL}/api`
+    : `${REACT_APP_PROD_API_URL}/api`;
 
 export const requestConfig = (url: string, method: string, data: any) => {
   let config;
